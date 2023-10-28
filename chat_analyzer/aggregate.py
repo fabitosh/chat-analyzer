ea62f7626e8e7539f779da3ff41bf49eba474485
Fabio Meier <fabitosh@gmail.com>
2023-10-28 20:08:39 +0200

    aggregate multiple chat rows of same user

---
+++ chat_analyzer/aggregate.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pandera.typing import DataFrame
+
+from chat_analyzer.types import RawChat, CombinedChat
+
+
+def merge_consecutive_msg(df: DataFrame[RawChat], merge_window_s: float = 60) -> DataFrame[CombinedChat]:
+    mask_time_delta = df['datetime'].diff() > pd.Timedelta(seconds=merge_window_s)
+    mask_same_sender = df["sender"].shift() != df["sender"]
+    mask_consecutive_messages = mask_time_delta | mask_same_sender
+    df_combined = df.groupby(mask_consecutive_messages.cumsum(), as_index=False).agg(
+        datetime=('datetime', 'first'),
+        sender=('sender', 'first'),
+        message=('message', '\n'.join),
+        n_block=('message', 'count'),
+        datetime_last=('datetime', 'last'),
+        receiver=('receiver', 'first'))
+    df_combined['block_duration'] = df_combined['datetime_last'] - df_combined['datetime']
+    return DataFrame[CombinedChat](df_combined)
+
+if __name__ == '__main__':
+    pass
