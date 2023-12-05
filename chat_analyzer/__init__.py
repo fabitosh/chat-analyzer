d5592dfd42681adf85f5a94734c24b3f54ef9c22
Fabio Meier <fabitosh@gmail.com>
2023-12-05 06:40:41 +0100

    set whatsapp src in project

--- chat_analyzer/__init__.py
+++ chat_analyzer/__init__.py
@@ -1,5 +1,5 @@
 MY_CHAT_NAMES = ["Fabio Meier"]
 PATH_SIGNAL_MSG = "data/raw/signal_messages/"
-PATH_WHATSAPP_MSG = r"data/raw/whatsapp_messages/"
+PATH_WHATSAPP_MSG = "data/raw/whatsapp_messages"
 
 PATH_DIR_PROCESSED_PICKLES = "data/processed/"
