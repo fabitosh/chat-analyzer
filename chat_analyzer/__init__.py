f45321c7c89818fa21e3b045a69ed9b5e3130e8c
Fabio Meier <fabitosh@gmail.com>
2024-01-28 21:33:38 +0100

    helper comments in configuration

--- chat_analyzer/__init__.py
+++ chat_analyzer/__init__.py
@@ -1,5 +1,9 @@
+# Message sender names, that should be mapped to you.
 MY_CHAT_NAMES = ["Fabio Meier"]
+
+# Paths to raw message folders
 PATH_SIGNAL_MSG = "data/raw/signal_messages/"
 PATH_WHATSAPP_MSG = "data/raw/whatsapp_messages/"
 
+# Export path for parsed and enriched pandas dataframes
 PATH_DIR_PROCESSED_PICKLES = "data/processed/"
