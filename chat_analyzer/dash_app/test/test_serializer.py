ce8d7589cda0d44c47e8c933f84fe00caa16a8fb
Fabio Meier <fabitosh@gmail.com>
2024-11-16 22:18:19 +0100

    test: verify improved compression

--- chat_analyzer/dash_app/test/test_serializer.py
+++ chat_analyzer/dash_app/test/test_serializer.py
@@ -28,3 +28,17 @@ def test_serialize_deserialize_retains_dtypes():
     deserialized = deserialize(serialized)
 
     assert df.equals(deserialized)
+
+
+def test_serialization_compresses_better_than_to_json():
+    n_rows = 500
+    df = pd.DataFrame({
+        'A': range(n_rows),
+        'B': [1.1 * i for i in range(n_rows)],
+        'C': ['red', 'blue'] + ['red'] * (n_rows - 2)
+    }).astype({'C': 'category'})
+    json_baseline_bytes = sys.getsizeof(df.to_json())
+
+    serialized = serialize(df)
+    size_serialized_bytes = sys.getsizeof(serialized)
+    assert size_serialized_bytes < json_baseline_bytes
